ae1cf24eb64f46a10d63c2cf24fb0ddff432e14d
Fredrik Hov Nilsen <nfredrikhov@gmail.com>
2024-06-06 17:43:58 +0200

    change footer in layout; polish footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,8 +4,47 @@ export const Footer = () => {
   return (
     <>
       <footer className={classNames(`${footerStyle.footer}`)}>
-        <div className={footerStyle.div}>Footer</div>
-        <p className={footerStyle.p}>Hei</p>
+        <nav className={footerStyle.nav}>
+          <ul className={footerStyle.footer_ul}>
+            <li className={footerStyle.footer_li}>
+              <a className={footerStyle.footer_a} href="#">
+                Hellu
+              </a>
+            </li>
+            <li className={footerStyle.footer_li}>
+              <a className={footerStyle.footer_a} href="#">
+                Hellu
+              </a>
+            </li>
+            <li className={footerStyle.footer_li}>
+              <a className={footerStyle.footer_a} href="#">
+                Hellu
+              </a>
+            </li>
+            <li className={footerStyle.footer_li}>
+              <img src="/footerFlower.svg" alt="Wedding logo" />
+            </li>
+
+            <li className={footerStyle.footer_li}>
+              <a className={footerStyle.footer_a} href="#">
+                Hellu
+              </a>
+            </li>
+            <li className={footerStyle.footer_li}>
+              <a className={footerStyle.footer_a} href="#">
+                Hellu
+              </a>
+            </li>
+            <li className={footerStyle.footer_li}>
+              <a className={footerStyle.footer_a} href="#">
+                Hellu
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className={footerStyle.div_lower}>
+          Copyright © 2024 Fredrik Hov Nilsen
+        </div>
       </footer>
     </>
   );
